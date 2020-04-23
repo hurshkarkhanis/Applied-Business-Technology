c55cfcb344508ae123052ecab0980daffe306d55
Hursh Karkhanis <pullupj7@gmail.com>
2020-04-23 11:30:43 -0700

    tried a bit more on problem 3

--- karkhanis_hursh_pa4.py
+++ karkhanis_hursh_pa4.py
@@ -1,4 +1,3 @@
-
 answer = 0
 for x in range (1, 101):
     answer += 1/x
@@ -24,24 +23,28 @@ average_after = int(sum(scores_list))/int(len(scores_list))
 print ("average after removing lowest value: ", round(average_after, 2))
 
 
-#not sure about problem 3
+#not sure about problem 3, but this is my logic:
 keep_going = True
 print("enter student's test scores here.")
 print("press 'x' to change student")
 print("press 'q' to end program")
 print("-------------------------")
+#list for scores of the current student
 current_student_scores = []
 
-while keep_going:
-    current_input = ("enter score here: ")
+while keep_going == True:
+    current_input = input("enter score here: ")
     if (current_input == 'x'):
         student_average = int(sum(current_student_scores))/int(len(current_student_scores))
         print("student average: ", student_average)
+        current_student_scores.clear()
+        #print currnt student averages and move on to new student
     else:
         if(current_input == 'q'):
             #exit loop
             keep_going = False
         else:
+            #add score to current student
             current_student_scores.append(current_input)
 
     
